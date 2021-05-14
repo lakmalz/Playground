7980b1ea6064eb26e728ea82ebaead7d423de95a
Lakmal Weerasekara <alrweerasekara@gmail.com>
2021-05-15 00:30:30 +0530

    added 2D array hourglass sum

--- app/src/main/java/com/lakmalz/playground/09_2DDimention.kt
+++ app/src/main/java/com/lakmalz/playground/09_2DDimention.kt
@@ -11,6 +11,10 @@ fun main(args: Array<String>) {
             intArrayOf(0, 0, 0, 2, 0, 0),
             intArrayOf(0, 0, 1, 2, 4, 0))
 
+    println(hourGlassSum(arr))
+}
+
+private fun hourGlassSum(arr: Array<IntArray>): Int {
     var rows = arr.size
     var columns = arr[0].size
 
@@ -18,10 +22,10 @@ fun main(args: Array<String>) {
 
     for (i in 0..(rows - 3)) {
         for (j in 0..(columns - 3)) {
-            val currentHourGlassSum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
+            val currentHourGlassSum =
+                arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2]
             maxHourGlassSum = max(maxHourGlassSum, currentHourGlassSum)
         }
     }
-
-    println(maxHourGlassSum)
+    return maxHourGlassSum
 }
